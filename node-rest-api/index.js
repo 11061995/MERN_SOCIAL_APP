914113df4bd8eb726b4688b14526876406fb0528
11061994 <tazeenkhan1106@gmail.com>
2021-11-05 00:53:36 +0500

    backend thread

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -7,21 +7,30 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const router = express.Router();
+const path = require("path");
+
 
 dotenv.config();
 
 mongoose.connect(
-        process.env.MONGO_URL, {
-            // useUrlParser: true,
-            // useFindAndModify: false,
-            useUnifiedTopology: true
-        }).then(() => console.log("DB Connected"))
-    .catch((err) => console.log(err));
+    process.env.MONGO_URL, {
+        // useUrlParser: true,
+        // useFindAndModify: false,
+        useUnifiedTopology: true,
+    },
+    () => {
+        console.log("Connected to MongoDB");
+    }
+);
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
